3ce97ff16e23abb0d42ff93f1948178612a025f7
Luca8991 <luca.berte98@hotmail.it>
2021-02-24 09:50:53 +0100

    improvements

--- pedro.py
+++ pedro.py
@@ -12,46 +12,42 @@ def tell_shift(day, date):
     delta = date - START
     next_shift = ''
     end_shift = ''
-    if delta.days in [0,1] or delta.days%8==0 or delta.days%9==0:
+    delta_days = delta.days%8
+    print(START, delta.days, delta.days//16, delta_days)
+    if delta_days in [0,1]:
         #print('adesso lavora al turno di mattina')
-        if delta.seconds <= 28800:
-            shift = 'mattina'
-            end_shift = 'finisce alle 15'
+        shift = '*mattina*'
+        end_shift = 'dalle *7* alle *15*'
+
+        if delta_days==0:
+            next_shift = 'lavora di mattina, dalle *7* alle *15*'
         else:
-            shift = 'none'
-        if delta.days==0 or delta.days%8==0:
-            next_shift = 'inizia alle 7, finisce alle 15'
-        else:
-            next_shift = 'inizia alle 15, finisce alle 23'
-    elif delta.days in [2,3] or delta.days%10==0 or delta.days%11==0:
+            next_shift = 'lavora di pomeriggio, dalle *15* alle *23*'
+    elif delta_days in [2,3]:
         #print(day+' lavora al turno di sera')
-        if delta.seconds > 28800 and delta.seconds <= 57600:
-            shift = 'pomeriggio'
-            end_shift = 'finisce alle 23'
+        shift = '*pomeriggio*'
+        end_shift = 'dalle *15* alle *23*'
+        
+        if delta_days==2:
+            next_shift = 'lavora di *pomeriggio*, dalle *15* alle *23*'
         else:
-            shift = 'none'
-        if delta.days==2 or delta.days%10==0:
-            next_shift = 'inizia alle 15, finisce alle 23'
-        else:
-            next_shift = 'inizia alle 23, finisce alle 7 del mattino dopo'
-    elif delta.days in [4,5] or delta.days%12==0 or delta.days%13==0:
+            next_shift = 'lavora di *notte*, delle *23* alle *7* del mattino dopo'
+    elif delta_days in [4,5]:
         #print(day+' lavora al turno di notte')
-        if delta.seconds > 57600 and delta.seconds <= 86399:
-            shift = 'night'
-            end_shift = 'finisce alle 15'
-        else:
-            shift = 'none'
-        if delta.days==4 or delta.days%12==0:
-            next_shift = 'inizia alle 23, finisce alle 7 del mattino dopo'
+        shift = '*notte*'
+        end_shift = 'dalle *23* alle *7*'
+        
+        if delta_days==4:
+            next_shift = 'lavora di *notte*, delle *23* alle *7* del mattino dopo'
         else:
             next_shift = 'è a casa'
-    elif delta.days in [6,7] or delta.days%14==0 or delta.days%15==0:
+    elif delta_days in [6,7]:
         #print(day+' non lavora')
         shift = 'none'
-        if delta.days==6 or delta.days%14==0:
+        if delta_days==6:
             next_shift = 'è a casa'
         else:
-            next_shift = 'inizia alle 7, finisce alle 15'
+            next_shift = 'lavora di *mattina*, dalle *7* alle *15*'
     return format_response(day, shift, end_shift, next_shift)
 
 def format_response(day, shift, end_shift, next_shift):
@@ -61,7 +57,7 @@ def format_response(day, shift, end_shift, next_shift):
     #OGGI lavora di _mattina_, _finisce alle 15_
     #DOMANI _inizia alle 15_
     if shift=='none':
-        resp_this_shift = 'non lavora'
+        resp_this_shift = '*non lavora*'
     else:
         resp_this_shift = 'lavora di '+shift
     
@@ -69,11 +65,11 @@ def format_response(day, shift, end_shift, next_shift):
         resp_this_shift += ', '+end_shift
 
     if day == 'oggi':
-        resp_this_shift = 'Oggi '+resp_this_shift
-        resp_next_shift += 'Domani '+next_shift
+        resp_this_shift = '_Oggi_ '+resp_this_shift
+        resp_next_shift += '_Domani_ '+next_shift
     if day == 'domani':
-        resp_this_shift = 'Domani '+resp_this_shift
-        resp_next_shift += 'Dopodomani '+next_shift
+        resp_this_shift = '_Domani_ '+resp_this_shift
+        resp_next_shift += '_Dopodomani_ '+next_shift
     
     return resp_this_shift + '\n' + resp_next_shift
 
@@ -100,13 +96,13 @@ class MessageHandler(telepot.aio.helper.ChatHandler):
                 if 'oggi' in text:
                     t = datetime.today()
                     shift_explain = tell_shift('oggi', t)
-                    await self.sender.sendMessage(shift_explain)
+                    await self.sender.sendMessage(shift_explain, parse_mode='markdown')
                 elif 'domani' in text:
                     t = datetime.today()
                     t = t + timedelta(1)
                     shift_explain = tell_shift('domani', t)
-                    await self.sender.sendMessage(shift_explain)
-                elif 'dio' in text:
+                    await self.sender.sendMessage(shift_explain, parse_mode='markdown')
+                elif ' dio ' in text:
                     await self.sender.sendMessage('dio non esiste, ma se esiste è un sadico di merda')
                 elif 'start' in text:
                     if chatInfo['username'] == 'ilbert98':
